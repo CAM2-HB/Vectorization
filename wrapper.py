de2a45b13e0290481d3f3d1e587fc9445c30a412
glasere@purdue.edu <42726565+ethanglaser@users.noreply.github.com>
2019-10-01 12:14:21 -0400

    Add files via upload

---
+++ wrapper.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import csv
+
+import cv2
+
+
+def compareSingle(outputPath, dataPath):
+	#run openface vectorization on each image in datapath and send output to outputpath
+	os.system("../OpenFace/OpenFace/build/bin/FaceLandmarkImg -fdir " + dataPath + " -out_dir " + outputPath)
+	maxconfidence = 0
+	for item in os.listdir(outputPath):
+		name = item.split('.')
+		#identify all csvs and find the highest confidence
+		if len(name) > 1 and name[1] == 'csv':
+			with open(outputPath + thing, 'r') as f:
+				data = list(csv.reader(f))
+			#confidence in second column, second row of csv
+			confidence = float(data[1][1])
+			if confidence > maxconfidence:
+				maxconfidence = confidence
+				maxname = name[0]
+	
+	#use opencv for analysis on the image associated with the highest confidence and print confidence onto the image and display
+	img = cv2.imread(outputPath + maxname + '.jpg')
+	cv2.putText(img, str(maxconfidence), (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 2.0, color=(0, 255, 255))
+	#displays image
+	cv2.imshow('image',img)
+	#saves image
+	cv2.imwrite('img.jpg', img)
+	cv2.waitKey(1000)
+	return
+
+
+def compareFolder(outputPath, dataPath):
+	#run openface vectorization on each image in datapath and send output to outputpath
+	for angle in os.listdir(dataPath):
+		os.system("../OpenFace/OpenFace/build/bin/FaceLandmarkImg -fdir " + dataPath + angle + " -out_dir " + outputPath + angle)
+	#for image in os.listdir(outputPath + angle):
+	#	for 
+	
+
+
+if __name__ == "__main__":
+	#specify directory that input data is located
+	dataPath = "../SampleData/SingleTestIMGs/00/"
+	#specify directory that output will be sent to
+	outputPath = "../Output/"
+	
+	compareSingle(outputPath, dataPath)
+	#compareFolder(outputPath, dataPath)
