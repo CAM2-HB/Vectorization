3229ff5a50255d5453c5edfda3f4d51cc2f10365
SerenaZZZZZ <lingzhoufusang@gmail.com>
2019-10-22 18:14:24 -0400

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -2,51 +2,51 @@ import os
 import sys
 import csv
 import shutil
-
+from itertools import islice
 import cv2
 
 
 def compareSingle(outputPath, dataPath):
-	#run openface vectorization on each image in datapath and send output to outputpath
-	os.system("../OpenFace/OpenFace/build/bin/FaceLandmarkImg -fdir " + dataPath + " -out_dir " + outputPath)
-	maxconfidence = 0
-	for item in os.listdir(outputPath):
-		name = item.split('.')
-		#identify all csvs and find the highest confidence
-		if len(name) > 1 and name[1] == 'csv':
-			with open(outputPath + item, 'r') as f:
-				data = list(csv.reader(f))
-			#confidence in second column, second row of csv
-			confidence = float(data[1][1])
-			if confidence > maxconfidence:
-				maxconfidence = confidence
-				maxname = name[0]
-	
-	#use opencv for analysis on the image associated with the highest confidence and print confidence onto the image and display
-	img = cv2.imread(outputPath + maxname + '.jpg')
-	cv2.putText(img, str(maxconfidence), (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 2.0, color=(0, 255, 255))
-	#displays image
-	cv2.imshow('image',img)
-	#saves image
-	cv2.imwrite('img.jpg', img)
-	cv2.waitKey(1000)
-	return
+    #run openface vectorization on each image in datapath and send output to outputpath
+    os.system("../build/bin/FaceLandmarkImg -fdir " + dataPath + " -out_dir " + outputPath)
+    maxconfidence = 0
+    for item in os.listdir(outputPath):
+        name = item.split('.')
+        #identify all csvs and find the highest confidence
+        if len(name) > 1 and name[1] == 'csv':
+            with open(outputPath + item, 'r') as f:
+                data = list(csv.reader(f))
+            #confidence in second column, second row of csv
+            confidence = float(data[1][1])
+            if confidence > maxconfidence:
+                maxconfidence = confidence
+                maxname = name[0]
+
+    #use opencv for analysis on the image associated with the highest confidence and print confidence onto the image and display
+    img = cv2.imread(outputPath + maxname + '.jpg')
+    cv2.putText(img, str(maxconfidence), (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 2.0, color=(0, 255, 255))
+    #displays image
+    cv2.imshow('image',img)
+    #saves image
+    cv2.imwrite('img.jpg', img)
+    cv2.waitKey(1000)
+    return
 
 
 def compareFolder(outputPath, dataPath, finalPath):
-	#run openface vectorization on each image in datapath and send output to outputpath
-	final = []
-	for angle in os.listdir(dataPath):
-		os.system("../OpenFace/OpenFace/build/bin/FaceLandmarkImg -fdir " + dataPath + angle + " -out_dir " + outputPath + angle)
-	#for each timeframe
-	for image in os.listdir(outputPath + angle):
-		imagedata = []
-		name = image.split('.')
-		#find jpgs
-		if len(name) > 1 and name[1] == 'jpg':
-			maxconfidence = 0
-			#compare jpgs across folders at each timeframe (img0000 in folder 1, 2, 3)
-			for folder in os.listdir(outputPath):
+    #run openface vectorization on each image in datapath and send output to outputpath
+    final = []
+    for angle in os.listdir(dataPath):
+        os.system("../build/bin/FaceLandmarkImg -fdir " + dataPath + angle + " -out_dir " + outputPath + angle)
+    #for each timeframe
+    for image in os.listdir(outputPath + angle):
+        imagedata = []
+        name = image.split('.')
+        #find jpgs
+        if len(name) > 1 and name[1] == 'jpg':
+            maxconfidence = 0
+            #compare jpgs across folders at each timeframe (img0000 in folder 1, 2, 3)
+            for folder in os.listdir(outputPath):
                 #check whether it's a folder or not
                 if os.path.isdir(outputPath + folder):
                     if name[0] + ".csv" in os.listdir(outputPath + folder):
@@ -57,40 +57,49 @@ def compareFolder(outputPath, dataPath, finalPath):
                         if confidence > maxconfidence:
                             maxconfidence = confidence
                             maxname = folder
-			#identifies image with highest confidence at each timeframe and adds it to finalPath, adds image ID, folder, confidence to final text file
-			#shutil.copy(outputPath + maxname + '/' + name[0] + '.jpg', finalPath)
-			imagedata = [name[0], maxname, maxconfidence]
+
+            
+            #identifies image with highest confidence at each timeframe and adds it to finalPath, adds image ID, folder, confidence to final text file
+#            shutil.copy(outputPath + maxname + '/' + name[0] + '.jpg', finalPath)
+            imagedata = [name[0][-4:], maxname, maxconfidence]
             final.append(imagedata)
-   
-           #print confidence onto the final images
-           img = cv2.imread(outputPath + maxname + '/' + name[0] + '.jpg')
-           cv2.putText(img, str(maxconfidence), (100, 300), cv2.FONT_HERSHEY_SIMPLEX, 2.0, color=(0, 255, 255))
-           #saves image
-           write_name = finalPath + '/' + name[0] + '.jpg'
-           cv2.imwrite(write_name, img)
-           cv2.waitKey(1000)
-           
-	with open(finalPath + 'final.txt', 'w') as f:
-		f.write("image, camera, confidence\n")
-		for data in final:
-			f.write(data[0] + ", " + data[1] + ", " + str(data[2]) + '\n')
+            
+            #print confidence onto the final images
+            img = cv2.imread(outputPath + maxname + '/' + name[0] + '.jpg')
+            cv2.putText(img, str(imagedata), (50, 300), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 255), 2)
+            #saves image
+            write_name = finalPath + '/' + name[0] + '.jpg'
+            cv2.imwrite(write_name, img)
+            cv2.waitKey(1000)
+            
+            
+    with open(finalPath + 'final.txt', 'w') as f:
+        f.write("image, camera, confidence\n")
+        for data in final:
+            f.write(data[0] + ", " + data[1] + ", " + str(data[2]) + '\n')
     #sort and print the finaldata
     a = []
+    print("image, camera, confidence")
     with open(finalPath + 'final.txt', 'r') as f:
-        for line in f:
+        for line in islice(f, 1, None): #skip the title line
             a.append(line.strip())
         for item in sorted(a):
             print(item)
-	return
-	
-
+    return
+    
 
 if __name__ == "__main__":
-	#specify directory that input data is located
-	dataPath = "../SampleData/SingleTestIMGs/"
-	#specify directory that output will be sent to
-	outputPath = "../Output/"
-	finalPath = '../Final/Ethan/'
-	
-	#compareSingle(outputPath, dataPath)
-	compareFolder(outputPath, dataPath, finalPath)
+    #clear the outputPath and finalPath before running the code
+    shutil.rmtree('../Output/')
+    os.mkdir('../Output/')
+    shutil.rmtree('../Final/Ethan/')
+    os.mkdir('../Final/Ethan/')
+    
+    #specify directory that input data is located
+    dataPath = "../mydata/"
+    #specify directory that output will be sent to
+    outputPath = "../Output/"
+    finalPath = '../Final/Ethan/'
+
+#    compareSingle(outputPath, dataPath)
+    compareFolder(outputPath, dataPath, finalPath)
